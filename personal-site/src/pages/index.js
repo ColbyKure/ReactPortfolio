dd3c326c8ee8e317e660e6b4344c90b3bb86f347
Colby Kure <colbykure@gmail.com>
2025-02-07 22:34:40 -0800

    add console.log for env

--- personal-site/src/pages/index.js
+++ personal-site/src/pages/index.js
@@ -2,6 +2,8 @@ import Hero from "../components/home/Hero";
 import ProjectGallery from "../components/home/ProjectGallery";
 
 const Home = () => {
+  console.log("ENV CHECK:", process.env.NEXT_PUBLIC_API_KEY);
+
   return (
     <div>
       <Hero />
